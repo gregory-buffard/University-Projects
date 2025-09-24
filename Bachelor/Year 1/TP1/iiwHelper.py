afad3e4c75bd263cb6ed0cbddc7013a3c1c4d240
Gregory Buffard <gregory442005@gmail.com>
2025-09-24 08:12:20 +0200

    Init TP1

---
+++ Bachelor/Year 1/TP1/iiwHelper.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+
+def display2DPoints(*lstPoints: list):
+    '''This function takes a list of Point2D as parameter and plot them by using the matplotlib module'''
+    for lstPoint in lstPoints:
+        xSet: list =[]
+        ySet: list = []
+        for p in lstPoint:
+            xSet = xSet + [p.x]
+            ySet = ySet + [p.y]    
+        plt.plot(xSet, ySet, marker='.')
+    plt.show()
